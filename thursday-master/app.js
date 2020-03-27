ebb0490e160e97a38d2aab009a70fce7bc97734e
Jaakko Hotti <jaakko.hotti@metropolia.fi>
2020-03-27 09:51:58 +0200

    m

---
+++ thursday-master/app.js
@@ -0,0 +1,15 @@
+'use strict';
+const cors=require('cors');
+const express = require('express');
+const app = express();
+const port = 3000;
+const catRoute = require('./routes/catRoute');
+const userRoute = require('./routes/userRoute');
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use('/cat', catRoute);
+app.use('/user', userRoute);
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
